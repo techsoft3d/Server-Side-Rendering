005b50129c9e1b100c7b62624110424cfb90907f
Ricky Johnson <ricky.johnson@techsoft3d.com>
2024-10-03 09:35:21 -0400

    changed to 2024

--- javascripts/common/load_scripts.js
+++ javascripts/common/load_scripts.js
@@ -4,7 +4,7 @@ const loadScripts = () => {
 
   // URL of files to load.
   const files = [
-    `https://cdn.jsdelivr.net/gh/techsoft3d/hoops-web-viewer@${HWP_VERSION}/hoops_web_viewer.js?v=${HWP_VERSION}`,
+    `https://cdn.jsdelivr.net/gh/techsoft3d/hoops-web-viewer@2024.4.0/hoops_web_viewer.js?v=2024.4.0`,
     "/javascripts/communicator_scripts/web_viewer_ui.js?v=latest",
     "/javascripts/communicator_scripts/communicator_server_integration.js?v=latest",
     "/javascripts/communicator_scripts/sample.js?v=latest",
